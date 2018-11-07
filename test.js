1af82fa9cfcea81958692e1399ffef14bf8a7445
Lucena <guilherme.lucena@mindresearch.org>
2018-11-07 20:08:32 -0200

    several bug fixes

--- test.js
+++ test.js
@@ -1,10 +1,24 @@
 require('.');
 
-let arr = [0,1,2,3,4,5,6,7,8,9];
+let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
-console.log(arr);
-console.log(arr.shuffle());
-console.log(arr.bubblesort());
-console.log(arr.sum());
+console.log('bubbleSort', arr.bubbleSort());
+console.log('shuffle', arr.shuffle());
+console.log('sum', arr.sum());
+console.log('getRandom', arr.getRandom());
+console.log('remove', arr.remove());
+console.log('clone', arr.clone());
+console.log('unique', arr.unique());
 
-console.log([0,1,1,1].unique());
+console.log((1234565789).formatMoney());
+console.log((5).factorial());
+
+let str = 'Uma opção alternativa ao café é o pó de guaraná.';
+let thisCouldBeBad = 'This¢£«±÷could&*()\/<>be!@#$%^bad';
+console.log('squeeze', str.squeeze());
+console.log('removeSymbols',thisCouldBeBad.removeSymbols());
+console.log('truncate', str.truncate());
+console.log('anagram', str.anagram());
+console.log('reverse', str.reverse());
+console.log('alphabeticalOrder', str.alphabeticalOrder());
+console.log('removeDiacritics', str.removeDiacritics());
